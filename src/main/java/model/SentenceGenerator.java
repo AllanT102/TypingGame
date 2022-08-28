0fcf232cb642ea3da7a395e2195ca7e764fa9443
AllanT102 <allan.zf.tan@gmail.com>
2022-08-27 17:48:29 -0700

    Finished sentence generator

--- src/main/java/model/SentenceGenerator.java
+++ src/main/java/model/SentenceGenerator.java
@@ -13,21 +13,27 @@ import java.util.List;
 
 public class SentenceGenerator {
 
-    public void getRandomSentences() {
+    public void getRandomSentences() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\allan\\chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver(options);
         driver.get("https://www.thewordfinder.com/random-sentence-generator/");
 
         WebElement numInput = driver.findElement(By.id("sentence_count"));
         WebElement generateBtn = driver.findElement(By.className("btn-lg"));
-        numInput.sendKeys(Keys.BACK_SPACE, "6");
+        numInput.sendKeys(Keys.RIGHT, Keys.BACK_SPACE);
+        numInput.sendKeys("9");
         generateBtn.click();
-        List<WebElement> sentences = driver.findElements(By.tagName("li"));
-        List<WebElement> a = new ArrayList<>();
-//        sentences.forEach(element -> a.add(element.findElement(By.tagName("a"))));
-        sentences.forEach(webElement -> System.out.println(webElement.getText()));
+        List<WebElement> sentences = driver.findElements(By.cssSelector("a[href='#']"));
+        List<WebElement> filteredSentences = new ArrayList<>();
+
+        sentences.forEach(webElement -> {
+            if (webElement.getAttribute("onclick").equals("bookmarkSentence(this);return(false);")) {
+                filteredSentences.add(webElement);
+            }
+        });
+        filteredSentences.forEach(webElement -> System.out.println(webElement.getText()));
         driver.quit();
     }
 
